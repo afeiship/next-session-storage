1d58a84bd2bd09831918ed0c5dc2034c20317807
afeiship <1290657123@qq.com>
2017-06-16 22:26:51 +0800

    update & finish

---
+++ src/next-session-storage.js
@@ -0,0 +1,24 @@
+(function () {
+
+  var global = global || this;
+
+  var nx = global.nx || require('next-js-core2');
+  var NxAbstractStorage = nx.AbstractStorage || require('next-abstract-storage');
+  var NxSessionStorage = nx.declare('nx.SessionStorage', {
+    extends: NxAbstractStorage,
+    methods:{
+      init: function(inPrefix){
+        this.base({
+          engine:'sessionStorage',
+          prefix: inPrefix || ''
+        });
+      }
+    }
+  });
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = NxSessionStorage;
+  }
+
+}());
